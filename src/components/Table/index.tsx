3cbcc92c8d09cdad927905b29c045a86a036ae7f
Emelly Plautz <emellyplautz@gmail.com>
2023-02-26 15:10:39 -0300

    I ended all connections with the API

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,29 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dataFormatter, priceFormatter } from '../../utils/formatter'
 import { SearchForme } from '../SearchForm'
 import { PriceHighlight, TableContainer, TransationTable } from './styles'
 
-interface Transaction {
-  id: number
-  description: string
-  type: 'income' | 'outcome'
-  price: number
-  category: string
-  createAT: string
-}
-
 export function Table() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
+  const { transactions } = useContext(TransactionsContext)
 
   return (
     <TableContainer>
@@ -36,11 +18,12 @@ export function Table() {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighlight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createAT}</td>
+                <td>{dataFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             )
           })}
---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,70 @@
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import { api } from '../lib/axios'
+
+interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
+
+interface createTransactionInput {
+  description: string
+  price: number
+  category: string
+  type: 'income' | 'outcome'
+}
+
+interface TransactionsContextType {
+  transactions: Transaction[]
+  fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (data: createTransactionInput) => Promise<void>
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode
+}
+
+export const TransactionsContext = createContext({} as TransactionsContextType)
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function fetchTransactions(query?: string) {
+    const response = await api.get('/transactions', {
+      params: {
+        _sort: 'createdAT',
+        _order: 'desc',
+        q: query,
+      },
+    })
+
+    setTransactions(response.data)
+  }
+
+  async function createTransaction(data: createTransactionInput) {
+    const response = await api.post('transactions', {
+      description: data.description,
+      price: data.price,
+      category: data.category,
+      type: data.type,
+      createdAt: new Date(),
+    })
+
+    setTransactions((state) => [response.data, ...state])
+  }
+
+  useEffect(() => {
+    fetchTransactions()
+  }, [])
+
+  return (
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransactions, createTransaction }}
+    >
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
